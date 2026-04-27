c252acb8ff21b51bfdf8135990ce6cde1236b3ca
unknown <987omar123osama456@gmail.com>
2026-04-27 14:21:57 +0300

    Fix Warranty expiry_date cast to date

--- app/Models/Warranty.php
+++ app/Models/Warranty.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Warranty extends Model
 {
-    protected $fillable = ['user_id','vehicle_id','title_ar','title_en','icon','provider','notes'];
-    protected $casts    = [];
+    protected $fillable = ['user_id','vehicle_id','title_ar','title_en','icon','expiry_date','provider','notes'];
+    protected $casts    = ['expiry_date' => 'date'];
 
     public function vehicle() { return $this->belongsTo(Vehicle::class); }
 }
