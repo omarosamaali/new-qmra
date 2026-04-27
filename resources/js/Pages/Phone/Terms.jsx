e546fac4e40e5dc36b31930b140a7ae6fadfd630
unknown <987omar123osama456@gmail.com>
2026-04-27 13:46:00 +0300

    omar

--- resources/js/Pages/Phone/Terms.jsx
+++ resources/js/Pages/Phone/Terms.jsx
@@ -21,7 +21,7 @@ export default function Terms({ privacyPolicy, termsAndConditions }) {
 
                     <div className="bg-white flex items-center gap-3 sticky top-0 z-20 shadow-sm safe-header">
                         <button
-                            onClick={() => router.get("/home")}
+                            onClick={() => router.get("/")}
                             className="w-9 h-17 flex items-center justify-center bg-[#800000] text-white active:opacity-80 transition-opacity"
                         >
                             <BackIcon />
