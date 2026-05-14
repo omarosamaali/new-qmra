d6444b63f23605c8bbd6e029201017f52dbc7794
unknown <987omar123osama456@gmail.com>
2026-05-14 12:06:23 +0300

    omar

--- resources/js/brand/assets.js
+++ resources/js/brand/assets.js
@@ -1,5 +1,7 @@
-import logoDark from "../../images/qmra-logo-dark.svg";
 import logoLight from "../../images/qmra-logo-light.svg";
 import iconFav from "../../images/qmra-icon.svg";
+
+/** Raster logo: add your file as `public/images/dark-logo.png` */
+const logoDark = "/images/dark-logo.png";
 
 export { logoDark, logoLight, iconFav };
